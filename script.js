440ffb06dcd389802437fb0e6c8374b8f20f7f7b
Mishaproh <mishaprohorenko96@gmail.com>
2021-04-22 20:55:18 +0300

    js

--- script.js
+++ script.js
@@ -26,57 +26,64 @@
 //     if (value === 1) return value;
 //     return value * factorial(value - 1)
 // }
-const developmentTeam = {
-    testing: [
-        {
-            name: 'Martina',
-            salary: 2000,
-        },
-        {
-            name: 'John',
-            salary: 300,
-        },
-    ],
-    developers: {
-        backEnd: [
-            {
-                name: 'Olga',
-                salary: 550,
-            }
-        ],
-        frontEnd: [
-            {
-                name: 'Roger',
-                salary: 1100,
-            },
-            {
-                name: 'Kaly',
-                salary: 1120,
-            },
-        ],
-    }
-}
+// const developmentTeam = {
+//     testing: [
+//         {
+//             name: 'Martina',
+//             salary: 2000,
+//         },
+//         {
+//             name: 'John',
+//             salary: 300,
+//         },
+//     ],
+//     developers: {
+//         backEnd: [
+//             {
+//                 name: 'Olga',
+//                 salary: 550,
+//             }
+//         ],
+//         frontEnd: [
+//             {
+//                 name: 'Roger',
+//                 salary: 1100,
+//             },
+//             {
+//                 name: 'Kaly',
+//                 salary: 1120,
+//             },
+//         ],
+//     }
+// }
 
-let getSalary = (team) => {
-    let sum = 0;
-    team.forEach(item => {
-        sum += item.salary;
-    })
-    return sum;
-};
 
-let getTeamSalary = (team) => {
-    if (Array.isArray(team)) {
-        return getSalary(team);
-    } else {
-        let sum = 0;
-        for (let key in team) {
-            sum += getTeamSalary(team[key]);
-        }
-        return sum;
-    }
-};
 
-console.log(getTeamSalary(developmentTeam))
+
+// let getSalary = (team) => {
+//     let sum = 0;
+//     team.forEach(item => {
+//         sum += item.salary;
+//     })
+//     return sum;
+// };
+
+
+
+// let getTeamSalary = (team) => {
+
+//     if (Array.isArray(team)) {
+//         return getSalary(team);
+//     } else {
+//         let sum = 0;
+//         for (let key in team) {
+//             sum += getTeamSalary(team[key]);
+
+//         }
+//         return sum;
+//     }
+// };
+
+// console.log(getTeamSalary(developmentTeam))
 
 
